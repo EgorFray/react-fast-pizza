c86feb6af9053be60f1f11c5b180991c37bf2b0b
EgorFray <fatamorganaa933@gmail.com>
2025-08-09 21:55:25 +0300

    Finish styling app using tailwindcss

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Button from "../../ui/Button";
 import LinkButton from "../../ui/LinkButton";
+import CartItem from "./CartItem";
 
 const fakeCart = [
   {
@@ -27,17 +28,25 @@ const fakeCart = [
 ];
 
 function Cart() {
-  // const cart = fakeCart;
+  const cart = fakeCart;
 
   return (
-    <div>
+    <div className="px-4 py-3">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2>Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
 
-      <div>
-        <Button to="/order/new">Order pizzas</Button>
-        <button>Clear cart</button>
+      <ul className="divide-y divide-stone-200 border-b border-stone-200">
+        {cart.map((item) => (
+          <CartItem item={item} key={item.key} />
+        ))}
+      </ul>
+
+      <div className="mt-6 space-x-2">
+        <Button to="/order/new" type="primary">
+          Order pizzas
+        </Button>
+        <Button type="secondary">Clear cart</Button>
       </div>
     </div>
   );
